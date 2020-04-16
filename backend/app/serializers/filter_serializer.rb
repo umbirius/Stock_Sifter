1c7d1b92d505e98fc51c4738ebcf11700810d6f3
umberto palazzo <upalazzo93@gmail.com>
2020-04-16 18:08:52 -0400

    added Filter Serializer

---
+++ backend/app/serializers/filter_serializer.rb
@@ -0,0 +1,5 @@
+class FilterSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :market_cap, :sector, :last_price, :fiftytwo_high, 
+    :fiftytwo_low, :vol, :avg_vol, :rel_vol, :insider_own, :inst_own
+end
